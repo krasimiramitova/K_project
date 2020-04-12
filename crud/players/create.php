bc953288c510b567ccdab24bab4f501afd5fe031
krisport <58785013+krisport@users.noreply.github.com>
2020-04-12 20:39:25 +0300

    Creating Sign up

--- crud/players/create.php
+++ crud/players/create.php
@@ -1,37 +1,73 @@
 <?php 
-include 'includes/header.php';
+
+  include '../includes/hangman_connect.php';
+
+
 ?>
 
 <form method="post" action="">
-	<?php
-			echo '<p>Enter new country </p>
-			<input type="text" name="country">
-			<input type="submit" name="submit" value="save">	
-		</form>';
-		
+	<p>Enter Username</p>
+	<input type="text" name="username">
+	<p>Enter Password</p>
+	<input type="password" name="password">
+    <p>Repeat Password</p>
+	<input type="password" name="verify_password">
+	<p>Enter Mail</p>
+	<input type="text" name="email">
+	<input type="submit" name="submit" value="Register">	
+</form>
+<?php
 
-//var_dump($_POST['country']);
-//1 
-	if (isset($_POST['country']))
-	{
-		$data = $_POST['country'];
+if( isset($_POST["submit"])){
+$flag = true;
+if( isset($_POST["username"])){
+	$username = $_POST["username"];
+}else{
+	$flag = false;
+	echo 'Enter Username!'; 
+}
+if (isset($_POST["password"])){
+	$password = $_POST["password"];
+}else{
+	$flag = false;
+	echo 'Enter password!';
+}
+if (isset($_POST["verify_password"])){
+	$verify_password = $_POST["verify_password"];
+}
+else{
+	$flag = false;
+}
+if(isset($password) && isset($verify_password)){
+if($password != $verify_password){
+	$flag = false;
+	echo 'Password doesnt match!';
+} 
+}
+if (isset($_POST["email"])) {
+	$email = $_POST["email"];
+}else{
+	$flag = false;
+	echo 'Enter Email!';
+}
+ if($flag){
+
+ $date_added =date("Y-m-d H:i:s");
+ $date_deleted =date("Y-m-d H:i:s");
 
 //2 insert_query
-		$insert_query = "INSERT INTO `".$table_name."`(`name`) VALUES ('$data')";
+$insert_query = "INSERT INTO `players`(`username`, `password`, `register_date`, `e-mail`, `date_deleted`) VALUES ('$username','$password','$date_added','$email','$date_deleted')";
+
 //3
-		$result = mysqli_query($conn, $insert_query);
+$result = mysqli_query($conn, $insert_query);
 
-//var_dump($result);
-	if($result)
-		{
-		echo 'Succesfully added '.$data;
-		}
- 	else 
-		{
-		die('Query failed!' . mysqli_error($conn));
-		}
-	}
-echo '<p></p><p><a href="index_countries.php" class="btn btn-warning">To base</a></p>';
-
-include 'includes/footer.php';
+if($result){
+	echo "Record created successfuly";
+} else {
+	die('Query failed!' . mysqli_error($conn));
+}
+}else{
+	echo 'Register Failed';
+}
+}
 ?>
