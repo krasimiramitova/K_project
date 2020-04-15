695925ee8a361d146428e0c721bb0bfce6af5259
krisport <58785013+krisport@users.noreply.github.com>
2020-04-15 21:25:55 +0300

    Login Ready

--- pages/en/login.php
+++ pages/en/login.php
@@ -2,26 +2,23 @@
 include '../includes/hangman_connect.php';
 
  ?>
-
-
-
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="utf-8">
 	<title>Login Page</title>
-	<link rel="stylesheet" href="style1.css">
+	<link rel="stylesheet" type="text/css" href="style1.css">
 </head>
 <body>
 	<div id="container">
 	<form method="post" action="">
 	<p>Enter Username</p>
-	<input type="text" name="username" class="user">
+	<input type="text" name="username">
 	<p>Enter Password</p>
-	<input type="password" name="password" class="pass">
+	<input type="password" name="password">
     <!-- <p>Verify Password</p>
 	<input type="password" name="verify_password"> -->
-	<input type="submit" id="btn" value="Login">
+	<input type="submit" name="submit" id="btn" value="Login">
 	</form>
 	</div>
 </body>
@@ -37,25 +34,24 @@ if( isset($_POST["username"])){
 	$flag = false;
 	echo 'Enter Username!'; 
 }
-if (isset($_POST["password"])){
+if(isset($_POST["password"])){
 	$password = $_POST["password"];
 }else{
 	$flag = false;
 	echo 'Enter password!';
 }
-}
-$select_query = 'SELECT * FROM `players` WHERE username =". $username ." AND password =". $password ."
-';
+
+$select_query = "SELECT * FROM `players` WHERE username ='". $username ."' AND password ='". $password ."'";
 
 $mysqli_result = mysqli_query($conn,$select_query);
-// var_dump($mysqli_result);
-// die();
+
 if(mysqli_num_rows($mysqli_result)){
 	echo 'You Have Successfully Logged in';
 	exit();
 }else{
 	echo 'You have Entered Incorrect Username or Password';
 	exit();
+}
 }
 ?>
 
